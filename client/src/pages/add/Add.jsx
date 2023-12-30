c2f2d37812bbf92bbde6400e098ddd8920b46ae6
sh4riwsh4 <semihugur4363@gmail.com>
2023-12-30 14:38:40 +0300

    İş oluşturma düzenlendi

--- client/src/pages/add/Add.jsx
+++ client/src/pages/add/Add.jsx
@@ -1,94 +1,26 @@
-import React, { useState } from "react"
-import "./Add.scss"
+import React from "react"
+import { useForm } from "react-hook-form"
+import { DevTool } from "@hookform/devtools";
 
 const Add = () => {
+  const { register, control, handleSubmit } = useForm({
+    mode: "onChange",
+  });
 
-    const [selectedItem, setSelectedItem] = useState(null);
-    const [isOpen, setIsOpen] = useState(true);
-  
-    const handleToggle = () => {
-      setIsOpen(!isOpen);
-    };
+  return (
+    <>
+      <form onSubmit={handleSubmit(d => console.log(d))}>
+        <h1>React Hook Form DevTools</h1>
 
-    const handleSelect = (item) => {
-        setSelectedItem(item);
-        setIsOpen(false);
-      };
+        <label>Test</label>
+        <input {...register("test")} />
 
-    return (
-        <div className="add">
-            <div className="add-container">
-                <div className="top">
-                    <div className="add-title">
-                        <p>İş Adı:</p>
-                        <input
-                        type="text"
-                        className="title-form"
-                        placeholder="İsim giriniz."
-                        required
-                        />      
-                    </div>
-                    <div className="add-description">
-                        <p>İş Detayları:</p>
-                        <input
-                        type="text"
-                        className="description-form"
-                        placeholder="Detayları giriniz."
-                        required
-                        />      
-                    </div>
-                    <div className="add-genre">
-                        <p>İş Türü:</p>
-                        <div className="dropdown">
-                        <button className="dropdown-toggle" onClick={handleToggle}>
-                            {selectedItem ? selectedItem : 'Seçiniz'}
-                        </button>
+        <input type="submit" />
+      </form>
+      
+      <DevTool control={control} /> {/* set up the dev tool */}
+    </>
+  );
+};
 
-                        {isOpen && (
-                            <ul className="dropdown-menu">
-                            <li onClick={() => handleSelect('Yazılım')}>Yazılım</li>
-                            <li onClick={() => handleSelect('Grafik Tasarım')}>Grafik Tasarım</li>
-                            <li onClick={() => handleSelect('Seslendirme')}>Seslendirme</li>
-                            </ul>
-                        )}
-                        </div>   
-                    </div>
-                    <div className="add-skills">
-                        <p>İstenen Beceriler:</p>
-                        <input
-                        type="text"
-                        className="skills-form"
-                        placeholder="Yetenekler"
-                        required
-                        />      
-                    </div>
-                    <div className="add-price">
-                        <p>Ücret:</p>
-                        <input
-                        type="text"
-                        className="price-form"
-                        placeholder="Ad"
-                        required
-                        />      
-                    </div>
-                    <div className="add-deadline">
-                        <p>Teslim Tarihi:</p>
-                        <input
-                        type="text"
-                        className="deadline-form"
-                        placeholder="Ad"
-                        required
-                        />      
-                    </div>
-                </div>
-                <div className="bot">
-                        <div className="create">
-                            <button className="createJob">İş Oluştur</button>
-                        </div>                   
-                </div>
-            </div>
-        </div>
-    )
-}
-
-export default Add
+export default Add;
