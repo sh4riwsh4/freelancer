39d69bf2c3dcbd8293552210646843d50287fbe1
sh4riwsh4 <semihugur4363@gmail.com>
2023-12-31 22:08:21 +0300

    mesajlar sekmesi yapıldı

---
+++ server/src/main/java/com/example/freelance/demo/start/Configure/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+package com.example.freelance.demo.start.Configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") // API endpoint'inizin path pattern'ını belirtin
+                .allowedOrigins("http://localhost:3000") // İzin verilen origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // İzin verilen HTTP metotları
+                .allowedHeaders("Content-Type", "Authorization"); // İzin verilen başlıklar
+    }
+}
